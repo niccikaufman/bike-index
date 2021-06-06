81a3992ec3e29862606f409a1909774d148cd918
Nicci Kaufman <niccikaufman@gmail.com>
2021-06-06 11:33:55 -0700

    add API call and response function

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+$(document).ready(function() {
+  $('#userLocation').click(function(){
+    const zipcode = $('#location').val();
+    $('#location').val("");
+    let request = new XMLHttpRequest();
+    const url = `https://bikeindex.org:443/api/v3/search/count?location=${zipcode}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`;
+        request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    request.open("GET",url,true);
+    request.send();
+    
+    function getElements(response){
+      console.log(response);
+    }
+  });
+});
