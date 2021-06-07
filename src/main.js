57b27a6daab424ab47aae201515a01da151466c7
Nicci Kaufman <niccikaufman@gmail.com>
2021-06-07 12:15:30 -0700

    allow user to input proximity

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import './css/styles.css';
 
 $(document).ready(function() {
   $('#userLocation').click(function(){
+    $('#results').val("");
+    $('#proximity-results').val("");
     const zipcode = $('#location').val();
+    const proximity = $('#proximity').val();
     $('#location').val("");
+    $('#proximity').val("");
     let request = new XMLHttpRequest();
-    const url = `https://bikeindex.org:443/api/v3/search/count?location=${zipcode}&distance=1&stolenness=stolen&access_token=${process.env.API_KEY}`;
+    const url = `https://bikeindex.org:443/api/v3/search/count?location=${zipcode}&distance=${proximity}&stolenness=stolen&access_token=${process.env.API_KEY}`;
         request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -20,8 +24,8 @@ $(document).ready(function() {
     
     function getElements(response){
       let total = response.stolen + response.non;
-      $("#results").append(`There are currently ${total} registered bikes in your area, ${response.stolen} of which have been declared stolen.`);
-      $("#proximity-results").append(`There are currently ${response.proximity} stolen bikes within one mile of your location.`);
+      $("#results").text(`There are currently ${total} registered bikes in your area, ${response.stolen} of which have been declared stolen.`);
+      $("#proximity-results").text(`There are currently ${response.proximity} stolen bikes within ${proximity} mile(s) of your location.`);
     }
   });
 });
