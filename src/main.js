1a6b84e7aa2ae91d421eb847246681847164feb9
Nicci Kaufman <niccikaufman@gmail.com>
2021-06-07 12:09:02 -0700

    add proximity results

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     const zipcode = $('#location').val();
     $('#location').val("");
     let request = new XMLHttpRequest();
-    const url = `https://bikeindex.org:443/api/v3/search/count?location=${zipcode}&distance=10&stolenness=stolen&access_token=${process.env.API_KEY}`;
+    const url = `https://bikeindex.org:443/api/v3/search/count?location=${zipcode}&distance=1&stolenness=stolen&access_token=${process.env.API_KEY}`;
         request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
@@ -21,6 +21,7 @@ $(document).ready(function() {
     function getElements(response){
       let total = response.stolen + response.non;
       $("#results").append(`There are currently ${total} registered bikes in your area, ${response.stolen} of which have been declared stolen.`);
+      $("#proximity-results").append(`There are currently ${response.proximity} stolen bikes within one mile of your location.`);
     }
   });
 });
